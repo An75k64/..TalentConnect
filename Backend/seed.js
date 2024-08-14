e4c51e8725a71f5bd571b54f9bdd97a8c0021886
Ankit Kumar <An75k64@gmail.com>
2024-08-14 13:19:06 +0530

    merged

--- Backend/seed.js
+++ Backend/seed.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/mydatabase'; // Replace with your MongoDB URI
+const mongoURI = "mongodb://127.0.0.1:27017/mydatabase"; // Replace with your MongoDB URI
 
 mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB:', err));
